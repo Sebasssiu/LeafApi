71cd7ea9c9d93d21a017d50913302a29ebeeda62
Sebasssiu <54748964+Sebasssiu@users.noreply.github.com>
2021-03-20 16:08:43 -0600

    fix: User Model

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth import get_user_model
 
 class Users(AbstractUser):
     artist_name = models.CharField(max_length=32, blank=True, default='')
@@ -11,17 +11,18 @@ class Users(AbstractUser):
     is_admin = models.BooleanField(default=False, blank=False)
     is_staff = models.BooleanField(default=False, blank=False)
 
+User = get_user_model()
 
 class Premium(models.Model):
     suscription_date = models.DateField(blank=False)
-    User = models.ForeignKey(Users, on_delete=models.CASCADE)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Album(models.Model):
     name = models.CharField(max_length=32, blank=False)
     artist_id = models.CharField(max_length=32, blank=False)
     release_date = models.DateField(blank=False)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Genre(models.Model):
@@ -33,18 +34,18 @@ class Song(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='songs')
     album_id = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='almbum_songs')
     is_active = models.BooleanField(default=False)
-    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     link = models.CharField(max_length=300, blank=False, default="null")
 
 
 class Listen(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     date = models.DateField(blank=False)
 
 
 class PlayList(models.Model):
-    owner = models.ForeignKey(Users, on_delete=models.CASCADE, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
     name = models.CharField(max_length=30, blank=False)
     songs = models.ManyToManyField(Song)
 
