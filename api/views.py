f20712cf336d4c0b10b00844e023ba572a051d57
Oscar Saravia <46576030+oscarsaravia@users.noreply.github.com>
2021-05-28 11:14:45 -0600

    feat: simulating songs and listens

--- api/views.py
+++ api/views.py
@@ -12,6 +12,7 @@ from operator import itemgetter
 from collections import defaultdict
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.db import connection
+import random
 
 User = get_user_model()
 
@@ -223,6 +224,39 @@ class AlbumViewSet(viewsets.ModelViewSet):
         serializer = ALbumDateSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    @action(detail=False, methods=['POST'])
+    def simulatingsongs(self, request):
+        user_id = request.data['modified_id']
+        request_date = request.data['date']
+        for_range = request.data['range']
+
+        raw_query = f'SET SESSION "user.id" = {user_id};'
+        cursor = connection.cursor()
+        cursor.execute(raw_query)
+
+        song_name = "Let it be"
+        song_link = "https://www.youtube.com/embed/QDYfEBY9NM4"
+
+        all_albums = Album.objects.all()
+        listed_albums = list(all_albums)
+        genre = Genre.objects.all()
+        listed_genres = list(genre)
+
+        for i in range(0, int(for_range)):
+            random_album = random.sample(listed_albums, 9)
+            random_genre = random.sample(listed_genres, 9)
+            album_id = random_album[0].id
+            genre_id = random_genre[0].id
+            selected_album = Album.objects.filter(id=album_id)
+            selected_genre = Genre.objects.filter(id=genre_id)
+            user = User.objects.get(id=selected_album[0].user.id)
+            Song.objects.create(name=song_name, genre=selected_genre.first(), album_id=selected_album.first(),
+                                is_active=True,
+                                user=user, link=song_link)
+
+        serializer = AlbumSerializer(selected_album, many=True)
+        return Response(serializer.data)
+
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
@@ -350,6 +384,34 @@ class ListenViewSet(viewsets.ModelViewSet):
             print(result[i][0].split(",")[1][:-1])
             response[result[i][0].split(",")[0][1:].replace('"', '')] = result[i][0].split(",")[1][:-1]
         return Response(response)
+
+    @action(detail=False, methods=['POST'])
+    def simulatinglistens(self, request):
+        user_id = request.data['modified_id']
+        request_date = request.data['date']
+        for_range = request.data['range']
+
+        raw_query = f'SET SESSION "user.id" = {user_id};'
+        cursor = connection.cursor()
+        cursor.execute(raw_query)
+
+        all_songs = Song.objects.all()
+        listed_songs = list(all_songs)
+        all_users = Users.objects.all()
+        listed_users = list(all_users)
+
+        for i in range(0, int(for_range)):
+            random_song = random.sample(listed_songs, 9)
+            random_user = random.sample(listed_users, 9)
+            song_id = random_song[0].id
+            userid = random_user[0].id
+            selected_song = Song.objects.filter(id=song_id)
+            selected_user = Users.objects.filter(id=userid)
+            Listen.objects.create(song=selected_song.first(), user=selected_user.first(), date=request_date)
+
+        response = {'message': 'Simulation finished'}
+        return Response(response, status=status.HTTP_200_OK)
+
 
 
 class AllSongViewSet(viewsets.ModelViewSet):
